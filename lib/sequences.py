19cefec938e5329d016f82549991369186fc60d3
RickMohamedFiri <rick.mohamed@student.moringaschool.com>
2023-08-27 18:54:21 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]  # Initialize the sequence with the first two Fibonacci numbers.
+
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence[:length])  # Print the first 'length' elements of the sequence.
+
+
+print_fibonacci(8)
